d23e0141b14cedbce5c6efaceca5400295aa36a7
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2023-11-23 18:12:20 -0500

    Definimos el tamano y los valores random

--- matriz_transpuesta.c
+++ matriz_transpuesta.c
@@ -3,3 +3,30 @@ dimensiones de un arreglo bidimensional (matriz),
 el programa llene la matriz con números aleatorios entre 0 y 100. 
 Luego, calcule la matriz transpuesta. 
 El programa debe imprimir la matriz original y la matriz transpuesta.*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int columas;
+    int filas;
+    printf("Ingrese las columnas de la matriz -> ");
+    scanf("%d", &columas);
+    printf("Ingrese las filas de la matriz -> ");
+    scanf("%d", &filas);
+
+    int matrix[filas][columas];
+
+    for (int i = 0; i < filas; i++)
+    {
+        for ( int j = 0; j < columas; j++)
+        {
+            matrix[i][j]=rand()%100;
+        }
+    }
+
+    
+    
+    return 0;
+}
