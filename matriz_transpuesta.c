ed6db286035bdff9c6597adee62af9e031d8c420
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2023-11-23 18:05:34 -0500

    Creacion de programa

---
+++ matriz_transpuesta.c
@@ -0,0 +1,5 @@
+/*Construir un programa en lenguaje C donde el usuario ingrese las 
+dimensiones de un arreglo bidimensional (matriz), 
+el programa llene la matriz con números aleatorios entre 0 y 100. 
+Luego, calcule la matriz transpuesta. 
+El programa debe imprimir la matriz original y la matriz transpuesta.*/
